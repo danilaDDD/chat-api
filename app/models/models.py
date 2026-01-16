daea254aa07572ac28cead121743febe855ba79f
danila nikitin <danila.n2015@yandex.ru>
2026-01-16 20:46:16 +0300

    rework migrations and recreate all models

--- app/models/models.py
+++ app/models/models.py
@@ -12,6 +12,9 @@ class Chat(AbsId, AbsCreated):
     title: str = Column(String(200), nullable=False)
     messages: Mapped[List["Message"]] = relationship("Message", back_populates="chat", cascade="all, delete-orphan")
 
+    def __repr__(self) -> str:
+        return f"<Chat: {self.title}>"
+
 
 class Message(AbsId, AbsCreated):
     __tablename__ = 'messages'
@@ -19,3 +22,6 @@ class Message(AbsId, AbsCreated):
     text: str = Column(Text, nullable=False)
     chat_id: int = Column(Integer, ForeignKey('chats.id'), nullable=False)
     chat: Mapped["Chat"] = relationship("Chat", back_populates="messages")
+
+    def __repr__(self):
+        return f"<Message: {self.text[:20]} in Chat ID {self.chat_id}>"
